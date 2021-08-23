59e0361f9a5d411ce4c9dab78f3e0e036df2a26f
jyyblue <jyyblue1987@outlook.com>
2021-08-23 11:19:49 +0800

    [add] add useeffect, initprops.

--- pages/profile.js
+++ pages/profile.js
@@ -2,16 +2,27 @@ import Layout from "../components/layout";
 import {useRouter} from "next/router";
 import {useEffect} from 'react';
 
-export default function Profile() {
+const Profile = (props) => {
     const router = useRouter();
-
     useEffect(() => {
-        console.log("useEffect");
+        console.log("useEffect", props);
+        fetch("/api/hello");
     }, []);
 
     return (
         <Layout>
-            <p>Hello, my name is {router.query.profile} seo, I use next.js</p>
+            <p>Hello, my name is {router.query.profile} seo, I use next.js {props.data1.name}</p>
         </Layout>
     )
 }
+
+
+Profile.getInitialProps = async function() {    
+    let res = await fetch("https://nextjs-ten-gamma-29.vercel/api/hello");
+    let data = await res.json();
+    return {
+        data1: data
+    };
+};
+
+export default Profile;
